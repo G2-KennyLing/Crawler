828eedbf05b5c0e346260f0dfbea5f93114709c7
Ling <llinht1999@gmail.com>
2021-05-04 11:18:46 +0700

    first commit

--- server.js
+++ server.js
@@ -1,25 +1,31 @@
 const cheerio = require('cheerio');
 const request = require('request-promise');
 const fs = require('fs');
+const { Parser } = require('json2csv');
 
-request('https://123job.vn/tuyen-dung', (error, response, html) => {
-  if(!error && response.statusCode == 200) {
-    const $ = cheerio.load(html);
-    let data = []
-    $('.job__list-item').each((index, el) => {
-      const job = $(el).find('.job__list-item-title a').text();
-      const company = $(el).find('.job__list-item-company span').text();
-      const address = $(el).find('.job__list-item-info').find('.address').text();
-      const salary = $(el).find('.job__list-item-info').find('.salary').text();
 
-      data.push({
-        job, company, address, salary
-      });
-    });
+request('https://99shirt.com/search?type=product&q=shirt*', (error, response, html) => {
+    if (!error && response.statusCode == 200) {
+        const $ = cheerio.load(html);
+        let data = []
+        $('.product-wrap').each((index, el) => {
+            const title = $(el).find(' a div.product-details span.title').text();
+            const image = 'http:' + $(el).find(' div.product_image a div.image__container img').data().src;
+            const price = $(el).find('a div.product-details span.price span span.money ').text();
+            const link = 'https://99shirt.com' + $(el).find(' a').get().map(pro => { return pro.attribs.href });
 
-    fs.writeFileSync('data.json', JSON.stringify(data));
-  }
-  else {
-    console.log(error);
-  }
+            data.push({
+                title,
+                image,
+                price,
+                link
+            });
+        });
+        const fields = [{ label: 'Title', value: 'title' }, { label: 'Link_Image_1', value: 'image' }, { label: 'Link', value: 'link' }, { label: 'Price', value: 'price' }];
+        const parser = new Parser(fields);
+        const csv = parser.parse(data);
+        fs.appendFileSync('newmoon_import_template.csv', csv);
+    } else {
+        console.log(error);
+    }
 });
