5a5beade56dbfd1296042e1e4517181ea6cd692f
Ling <llinht1999@gmail.com>
2021-05-11 22:48:47 +0700

    Edit while to for

--- crawler.js
+++ crawler.js
@@ -10,17 +10,25 @@ const proxy = ["http://30c0fd28ae:LbSYfc4J@162.244.144.79:4444", "http://1a64c2d
 const crawlerPageCollection = async(link) => {
     try {
         const data = [];
+        let products_count = 0;
+        let urlInput = link + '/products.json?limit=250&page=';
         let page = 1;
-        let urlInput = link + '/products.json?limit=1000&page=';
-        let check = true;
-        while (check) {
+        if (link.length == 0) console.log("Link is not found");
+        await request.defaults({
+                'headers': { 'User-Agent': userAgent[Math.floor(Math.random() * userAgent.length)] }
+            }, { 'proxy': proxy[Math.floor(Math.random() * userAgent.length)] })
+            .get(link + '.json', (error, response, body) => {
+                products_count = JSON.parse(body).collection.products_count;
+                if (products_count === 0) return console.log('Page not have data');
+            })
+        page += Math.floor(products_count / 250);
+        for (let i = 1; i <= page; i++) {
             await request.defaults({
                     'headers': { 'User-Agent': userAgent[Math.floor(Math.random() * userAgent.length)] }
                 }, { 'proxy': proxy[Math.floor(Math.random() * userAgent.length)] })
-                .get(urlInput + page, (error, response, body) => {
-                    if (link.length == 0) console.log("Link is not found");
+                .get(urlInput + i, (error, response, body) => {
                     if (!error && response.statusCode >= 200 && response.statusCode <= 300) {
-                        if (JSON.parse(body).products.length == 0) check = false;
+                        if (JSON.parse(body).products.length === 0) return 0;
                         for (let product of JSON.parse(body).products) {
                             const obj = {
                                 Title: '',
@@ -43,9 +51,7 @@ const crawlerPageCollection = async(link) => {
                             obj.tags = product.tags + "";
                             data.push(obj);
                         }
-                        if (data.length == 0) check = false;
-                        console.log(`Crawling page: ${urlInput + page}`);
-                        page++;
+                        console.log(`Crawling page: ${urlInput + i}`);
                     } else console.log(error)
                 })
         }
@@ -59,9 +65,10 @@ const crawlerPageCollection = async(link) => {
 const crawlerPageSeach = async(link) => {
     let num = 0;
     let page = '';
-    let urlInput = link;
-    let sumPro = 0;
-    request(link, (error, response, html) => {
+    let urlInput = link + '&page=';
+    let data = [];
+    if (link.length == 0) return console.log("Link is not found");
+    await request(link, (error, response, html) => {
         if (!error && response.statusCode >= 200 && response.statusCode <= 300) {
             const $ = cheerio.load(html);
             const paging = $(".paginate span ").text().split('');
@@ -74,84 +81,70 @@ const crawlerPageSeach = async(link) => {
             page = page.split('').reverse().join('');
         }
     })
-    const crawler = async(link) => {
-        let data = [];
-        try {
-            await request.defaults({
-                    'headers': { 'User-Agent': userAgent[Math.floor(Math.random() * userAgent.length)] }
-                }, { 'proxy': proxy[Math.floor(Math.random() * userAgent.length)] })
-                (
-                    link,
-                    async(error, response, html) => {
-                        if (link.length == 0) return console.log("Link is not found");
-                        if (!error && response.statusCode >= 200 && response.statusCode <= 300) {
-                            const $ = cheerio.load(html);
-                            $(".product-wrap").each(async(index, el) => {
-                                const Title = $(el)
-                                    .find(" a div.product-details span.title")
-                                    .text();
-                                const Link_Image1 =
-                                    "http:" +
-                                    $(el).find("div.product_image a div.image__container img").data().src;
-                                const Link_Image2 = "http:" +
-                                    ($(el).find(" div.product_image a div.image__container img.secondary")).get().map((pro) => { return pro.attribs.src });
-                                let Link =
-                                    "https://99shirt.com" +
-                                    $(el)
-                                    .find(" a")
-                                    .get()
-                                    .map((pro) => {
-                                        return pro.attribs.href;
-                                    });
-                                Link = Link.slice(0, Link.indexOf("?"));
-                                const obj = {
-                                    Title,
-                                    Link_Image1,
-                                    Link_Image2,
-                                    Link_Image3: '',
-                                    Link_Image4: '',
-                                    SKU: '',
-                                    Link,
-                                    mmolazi_type: '',
-                                    tags: '',
-                                    category: '',
-                                }
-
-                                await request.defaults({
-                                    'headers': { 'User-Agent': userAgent[Math.floor(Math.random() * userAgent.length)] }
-                                }, { 'proxy': proxy[Math.floor(Math.random() * userAgent.length)] })(Link, async(error, response, html) => {
-                                    if (response.statusCode >= 200 && response.statusCode <= 300) {
-                                        const $ = cheerio.load(html);
-                                        const Link_Image3 = "http:" + $("div.gallery-cell ").eq(2).find("img").get().map((pro) => { return pro.attribs.src });
-                                        const Link_Image4 = "http:" + $("div.gallery-cell ").eq(3).find("img").get().map((pro) => { return pro.attribs.src });
-                                        obj.Link_Image3 = Link_Image3;
-                                        obj.Link_Image4 = Link_Image4;
-                                        await data.push(obj);
-
-                                    } else console.log(error)
-                                })
-                            });
-                            if (num == +page) {
-                                return 0;
+    page = +page;
+    for (let i = 1; i <= page; i++) {
+        await request.defaults({
+                'headers': { 'User-Agent': userAgent[Math.floor(Math.random() * userAgent.length)] }
+            }, { 'proxy': proxy[Math.floor(Math.random() * userAgent.length)] })
+            (
+                urlInput + i,
+                async(error, response, html) => {
+                    if (!error && response.statusCode >= 200 && response.statusCode <= 300) {
+                        const $ = cheerio.load(html);
+                        $(".product-wrap").each(async(index, el) => {
+                            const Title = $(el)
+                                .find(" a div.product-details span.title")
+                                .text();
+                            const Link_Image1 =
+                                "http:" +
+                                $(el).find("div.product_image a div.image__container img").data().src;
+                            const Link_Image2 = "http:" +
+                                ($(el).find(" div.product_image a div.image__container img.secondary")).get().map((pro) => { return pro.attribs.src });
+                            let Link =
+                                urlInput.split('/search')[0] +
+                                $(el)
+                                .find(" a")
+                                .get()
+                                .map((pro) => {
+                                    return pro.attribs.href;
+                                });
+                            Link = Link.slice(0, Link.indexOf("?"));
+                            const obj = {
+                                Title,
+                                Link_Image1,
+                                Link_Image2,
+                                Link_Image3: '',
+                                Link_Image4: '',
+                                SKU: '',
+                                Link,
+                                mmolazi_type: '',
+                                tags: '',
+                                category: '',
                             }
-                            num++;
-                            await crawler(link + '&page=' + num);
-                            writeFile(data, csvf);
-                            sumPro += data.length;
-                            console.log(`Have ${sumPro} products crawled from: ${urlInput}`);
-                        } else {
-                            console.log(error);
-                        }
+
+                            await request.defaults({
+                                'headers': { 'User-Agent': userAgent[Math.floor(Math.random() * userAgent.length)] }
+                            }, { 'proxy': proxy[Math.floor(Math.random() * userAgent.length)] })(Link, async(error, response, html) => {
+                                if (response.statusCode >= 200 && response.statusCode <= 300) {
+                                    const $ = cheerio.load(html);
+                                    const Link_Image3 = "http:" + $("div.gallery-cell ").eq(2).find("img").get().map((pro) => { return pro.attribs.src });
+                                    const Link_Image4 = "http:" + $("div.gallery-cell ").eq(3).find("img").get().map((pro) => { return pro.attribs.src });
+                                    obj.Link_Image3 = Link_Image3;
+                                    obj.Link_Image4 = Link_Image4;
+                                    await data.push(obj);
+                                } else console.log(error)
+                            })
+                        });
+                        console.log(`Crawling page: ${urlInput + i}`);
+                    } else {
+                        console.log(error);
                     }
-                );
-            console.log(`Crawling page: ${urlInput}&page=${num}`);
+                }
+            )
+    }
+    writeFile(data, csvf);
+    console.log(`Have ${data.length} products crawled from: ${link}`);
 
-        } catch (error) {
-            console.log(error);
-        }
-
-    };
-    await crawler(link);
 }
 
 const CrawlerProduct = async(link) => {
@@ -231,13 +224,13 @@ const writeFile = async(data, csvf) => {
 
 }
 const input = readFileInput("./input.txt");
-const main = async(input, csvf) => {
+const main = async(input) => {
     try {
         for (link of input) {
             const arr = link.split("/");
-            if (arr[3] === 'collections') crawlerPageCollection(link);
-            if (arr[3] === 'products') CrawlerProduct(link);
-            if (arr[3].split('?')[0] === 'search') crawlerPageSeach(link);
+            if (arr[3] === 'collections') crawlerPageCollection(link)
+            else if (arr[3] === 'products') CrawlerProduct(link)
+            else if (arr[3].split('?')[0] === 'search') crawlerPageSeach(link);
         }
     } catch (error) {
         console.log(error)
